ad82cff77c51d5c7f516095cba0c2696b1c3575c
Joel Quimis <quimisjoedl@gmail.com>
2025-05-08 21:18:45 -0500

    Primer - Documentado

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,14 +1,21 @@
+// Importamos el hook useState de React y los componentes necesarios
 import { useState } from "react"
 import { AddCotegory, GifGrid } from "./components";
 
+// Componente principal de la aplicació
 export  const GifExpertApp = () => {
+    // Hook de estado para almacenar las categorías. Se inicia con 'one punch'
     const [categories, setCategories] = useState(['one punch']);
+    // Función que se encarga de agregar una nueva categoría
     const onAddCategoria = (newCategory) => {
         //Vegeta
         //categories.push(newCategory)
+        // Si la categoría ya existe, no se agrega (evita duplicados)
         if(categories.includes(newCategory))return;
+        // Agrega la nueva categoría al inicio del arreglo
         setCategories([ newCategory, ...categories]);
     }
+     // Render del componente
 return (
     <>
         <h1>GifExpertApp</h1>
