5fe55f456e39e2af4bf29cd65f40ab9816e22cf7
Joel Quimis <quimisjoedl@gmail.com>
2025-06-19 16:35:59 -0500

    Actualizado

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,34 +1,31 @@
 // Importamos el hook useState de React y los componentes necesarios
-import { useState } from "react"
+import { useState } from "react";
 import { AddCotegory, GifGrid } from "./components";
 
 // Componente principal de la aplicació
-export  const GifExpertApp = () => {
-    // Hook de estado para almacenar las categorías. Se inicia con 'one punch'
-    const [categories, setCategories] = useState(['one punch']);
-    // Función que se encarga de agregar una nueva categoría
-    const onAddCategoria = (newCategory) => {
-        //Vegeta
-        //categories.push(newCategory)
-        // Si la categoría ya existe, no se agrega (evita duplicados)
-        if(categories.includes(newCategory))return;
-        // Agrega la nueva categoría al inicio del arreglo
-        setCategories([ newCategory, ...categories]);
-    }
-     // Render del componente
-return (
+export const GifExpertApp = () => {
+  // Hook de estado para almacenar las categorías. Se inicia con 'one punch'
+  const [categories, setCategories] = useState(["One Punch"]);
+  // Función que se encarga de agregar una nueva categoría
+  const onAddCategoria = (newCategory) => {
+    //Vegeta
+    //categories.push(newCategory)
+    // Si la categoría ya existe, no se agrega (evita duplicados)
+    if (categories.includes(newCategory)) return;
+    // Agrega la nueva categoría al inicio del arreglo
+    setCategories([newCategory, ...categories]);
+  };
+  // Render del componente
+  return (
     <>
-        <h1>GifExpertApp</h1>
-        <AddCotegory
+      <h1>GifExpertApp</h1>
+      <AddCotegory
         //setCategories = {setCategories}
-            onNewCategory = {event=> onAddCategoria(event)}
-        />
-            {
-                categories.map ( ( category ) => (
-                    <GifGrid key = {category} category={category} />
-                ))
-                
-            }
-        </>
-)
-}
+        onNewCategory={(event) => onAddCategoria(event)}/>
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url= `https://api.giphy.com/v1/gifs/search?api_key=u257lZ0YWKWY6zroLzv4c7KuphjsDMan&q=${category}&limit=9`
+    const url= `https://api.giphy.com/v1/gifs/search?api_key=u257lZ0YWKWY6zroLzv4c7KuphjsDMan&q=${category}&limit=6`
     const resp= await fetch(url)
     const {data} = await resp.json()
     
